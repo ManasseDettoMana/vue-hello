1ceca10075941a33dc3ac87ad7bdbbe61c140368
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-14 16:32:18 +0200

    script finito

---
+++ script.js
@@ -0,0 +1,10 @@
+var app = new Vue(
+    {
+        el:'#root',
+        data:{
+            hW: 'Hello',
+            immagine:'https://c.tenor.com/g90dduLXHtAAAAAM/allo-worm.gif',
+            citazione:'Si, è una citazione a labyrinth che ho visto tante volte. :)'
+        }
+    }
+);
